2a60e1a6efdb1664bf79350a2e96176bfd53b395
yiyangyi <yiyangyigg@gmail.com>
2015-03-10 21:28:56 +0800

    Trying the morphs api.

--- database/migrations/2015_03_09_080805_create_votes_table.php
+++ database/migrations/2015_03_09_080805_create_votes_table.php
@@ -15,6 +15,9 @@ class CreateVotesTable extends Migration {
 		Schema::create('votes', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->integer('user_id')->unsigned()->index();
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+			$table->morphs('votable')->index();
 			$table->timestamps();
 		});
 	}
