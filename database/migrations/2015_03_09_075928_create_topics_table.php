1451cec4602e5a1eff79b48a68288165fb8b71e7
yiyangyi <yiyangyigg@gmail.com>
2015-03-10 19:18:12 +0800

    Add some fields to the topics migration files.

--- database/migrations/2015_03_09_075928_create_topics_table.php
+++ database/migrations/2015_03_09_075928_create_topics_table.php
@@ -15,6 +15,18 @@ class CreateTopicsTable extends Migration {
 		Schema::create('topics', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->string('title')->index();
+			$table->text('body');
+			$table->integer('user_id')->index();
+			$table->integer('node_id')->index();
+			$table->boolean('excellent')->default(false)->index();
+			$table->boolean('blocked')->default(false)->index();
+			$table->integer('reply_count')->default(0)->index();
+			$table->integer('view_count')->default(0)->index();
+			$table->integer('favorite_count')->default(0)->index();
+			$table->integer('vote_count')->default(0)->index();
+			$table->integer('last_reply_user_id')->default(0)->index();
+			$table->softDeletes();
 			$table->timestamps();
 		});
 	}
