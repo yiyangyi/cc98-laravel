a24e48242bf8aa108fa42c03d2a950c63cad58e0
yiyangyi <yiyangyigg@gmail.com>
2015-03-11 20:08:57 +0800

    Polymorphic relations of vote.

--- app/Vote.php
+++ app/Vote.php
@@ -4,6 +4,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vote extends Model {
 
-	//
+	//A user can vote both a topic or a reply
+	
+	protected $fillable = [
+		'user_id',
+		'votable_id',
+		'votable_type',
+	];
+
+	public function votable()
+	{
+		return $this->morphTo();
+	};
+
+	public function scopeByWhom($query, $user_id)
+	{
+		return $query->where('user_id', '=', $user_id);
+	};
+
+	public function scopeWithType($query, $type)
+	{
+		return $query->where('votable_type', '=', $type);
+	};
 
 }
