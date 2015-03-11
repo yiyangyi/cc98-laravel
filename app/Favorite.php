253892e9389a7c6c1410beb3e23b4b6e23b6eeef
yiyangyi <yiyangyigg@gmail.com>
2015-03-11 19:54:26 +0800

    Is this the pivot model between user and topics.

--- app/Favorite.php
+++ app/Favorite.php
@@ -4,6 +4,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model {
 
-	//
+	protected $fillable = [];
+
+	public function user() 
+	{
+		return $this->belongsTo('User');
+	}
+
+	public function topic()
+	{
+		return $this->belongsTo('Topic');
+	}
+
+	public static function isUserFavoriteTopic(User $user, Topic $topic)
+	{
+		return Favorite::where('user_id', $user->id)
+					   ->where('topic_id', $topic->id)
+					   ->first();
+	}
 
 }
