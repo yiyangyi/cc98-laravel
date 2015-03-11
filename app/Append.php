1153bffa42f357fbfbc1748897c778533c80fbb2
yiyangyi <yiyangyigg@gmail.com>
2015-03-11 19:41:55 +0800

    Wired the relationship between topic and append.

--- app/Append.php
+++ app/Append.php
@@ -4,6 +4,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Append extends Model {
 
-	//
+	protected $fillable = [
+		'topic_id',
+		'content',
+	];
+
+	public function topic()
+	{
+		return $this->belongsTo('Topic');
+	}
 
 }
