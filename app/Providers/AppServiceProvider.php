1a0797f4f70d6ef6029cef5468a12fd107fc47fa
yiyangyi <yiyangyigg@gmail.com>
2015-03-13 18:33:43 +0800

    Shared some methods with all views that are rendered by the application.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,8 @@ class AppServiceProvider extends ServiceProvider {
 	 */
 	public function boot()
 	{
-		//
+		View::share('currentUser', Auth::user());
+		View::share('siteTip', Tip::getRandomTip());
 	}
 
 	/**
