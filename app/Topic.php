4a65f7b377100ef5ef8b33cfe73a2e34aa77d487
yiyangyi <yiyangyigg@gmail.com>
2015-03-14 13:25:21 +0800

    Set the relationships for the Topic model.

--- app/Topic.php
+++ app/Topic.php
@@ -4,6 +4,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model {
 
-	//
+	protected $fillable = [];
+
+	public function node()
+	{
+		return $this->belongsTo('Node');
+	}
+
+	public function user()
+	{
+		return $this->belongsTo('User');
+	}
+
+	public function replies()
+	{
+		return $this->hasMany('Reply');
+	}
+
+	public function appends()
+	{
+		return $this->hasMany('Append');
+	}
+
+	public function votes()
+	{
+		return $this->morphMany('Vote', 'votable');
+	}
+
+	public function favoriteBy()
+	{
+		return $this->belongsToMany('User', 'favorites');
+	}
+
+	public function attentedBy()
+	{
+		return $this->belongsToMany('User', 'attentions');
+	}
 
 }
