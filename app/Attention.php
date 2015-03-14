e6a539d2a5df6da20eaa6a75c144d6932568fa19
yiyangyi <yiyangyigg@gmail.com>
2015-03-14 12:02:27 +0800

    Set the relations of attention.

--- app/Attention.php
+++ app/Attention.php
@@ -4,6 +4,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attention extends Model {
 
-	//
+	protected $fillable = [];
+
+	public function post()
+	{
+		return $this->belongsTo('Post');
+	}
+
+	public function user()
+	{
+		return $this->belongsTo('User');
+	}
 
 }
