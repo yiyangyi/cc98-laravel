7edd4d5f3ae09c78590ffc5f2430095c62f17607
yiyangyi <yiyangyigg@gmail.com>
2015-03-14 13:35:09 +0800

    Set the relationships for the notification model.

--- app/Notification.php
+++ app/Notification.php
@@ -4,6 +4,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model {
 
-	//
+	protected $fillable = [
+		'body',
+		'type',
+		'topic_id',
+		'reply_id',
+		'user_id',
+		'from_user_id',
+	];
+
+	public function user()
+	{
+		return $this->belongsTo('User');
+	}
+
+	public function topic()
+	{
+		return $this->belongsTo('Topic');
+	}
+
+	public function fromUser()
+	{
+		return $this->belongsTo('User', 'from_user_id');
+	}
 
 }
