36e41bcc613bc7bc48f4d03bad34f8eab02cdbfd
yiyangyi <yiyangyigg@gmail.com>
2015-03-15 21:12:40 +0800

    Added the SoftDelete trait to add the deleted_at timestamp to set on the record.

--- app/User.php
+++ app/User.php
@@ -5,10 +5,15 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
 	use Authenticatable, CanResetPassword;
+
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 
 	/**
 	 * The database table used by the model.
