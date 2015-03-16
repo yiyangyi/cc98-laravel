1caa53471773817c003dfba5ad99624e0008615a
yiyangyi <yiyangyigg@gmail.com>
2015-03-16 23:48:23 +0800

    Add getRamdomTip static method to the tip model to get random tip.

--- app/Tip.php
+++ app/Tip.php
@@ -1,9 +1,23 @@
 <?php namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Tip extends Model {
 
-	//
+    const CACHE_KEY = 'tips';
+    const CACHE_MINUTES = 1440;
+
+	protected $fillable = ['body'];
+
+    public static function getRandomTip()
+    {
+        $tips = Cache::remember(self::CACHE_KEY, self::CACHE_MINUTES, function()
+        {
+            return Tip::all();
+        });
+
+        return $tips->random();
+    }
 
 }
