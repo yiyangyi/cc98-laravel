9c510a22e0b2c3cdf42c06894abbc69482ee9bc7
yiyangyi <yiyangyigg@gmail.com>
2015-03-18 19:32:59 +0800

    Add the actual markdown service.

---
+++ app/Services/Markdown.php
@@ -0,0 +1,24 @@
+<?php namespace App\Services;
+
+class Markdown {
+
+    public function __construct()
+    {
+
+    }
+
+    public function convertHtmlToMarkdown($html)
+    {
+
+    }
+
+    public function convertMarkdownToHtml($markdown)
+    {
+
+    }
+
+}
+
+
+
+
