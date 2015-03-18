d9a3362483c1d41ed601b3fa81b7587bf95d270f
yiyangyi <yiyangyigg@gmail.com>
2015-03-18 18:44:56 +0800

    Add the method to require permission or throw an exception.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,9 +3,18 @@
 use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\Auth;
+use Zizaco\Entrust\Entrust;
 
 abstract class Controller extends BaseController {
 
 	use DispatchesCommands, ValidatesRequests;
+
+    public function adminOrAuthorPermissionRequired($user_id)
+    {
+        if (!Entrust::can('manage_topics') && $user_id != Auth::user()->id) {
+            abort(401, "Admin or author permission required!");
+        }
+    }
 
 }
