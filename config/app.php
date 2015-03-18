a3ba38640226f5fdb20a499aac23d7b594cf187f
yiyangyi <yiyangyigg@gmail.com>
2015-03-18 19:03:59 +0800

    Register the markdown service provider in the providers array.

--- config/app.php
+++ config/app.php
@@ -144,6 +144,7 @@ return [
 		'App\Providers\ConfigServiceProvider',
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
+        'App\Providers\MarkdownServiceProvider',
 
 		'Clockwork\Support\Laravel\ClockworkServiceProvider',
 		'Zizaco\Entrust\EntrustServiceProvider',
