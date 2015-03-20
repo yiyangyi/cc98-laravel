db7ac3e1ac23c1cd08b09a323622472392bbc8ad
yiyangyi <yiyangyigg@gmail.com>
2015-03-20 10:05:07 +0800

    Add the logic to storing a reply and updating the attributes of the relevant topic and user.

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -3,7 +3,13 @@
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Http\Requests\MakeReplyRequest;
+use App\Reply;
+use App\Topic;
+use App\Services\Markdown;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RepliesController extends Controller {
 
@@ -32,9 +38,22 @@ class RepliesController extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function store()
+	public function store(MakeReplyRequest $request, Reply $reply, Topic $topic, Markdown $markdown)
 	{
-		//
+		$user_id = Auth::user()->id;
+        $body_original = $request->input('body');
+        $body = $markdown->convertMarkdownToHtml();
+        $reply = Reply::create(['body' => $body,
+                                'body_original' => $body_original,
+                                'user_id' => $user_id]);
+
+        $topic = Topic::find($reply->topic_id);
+        $topic->last_reply_user_id = Auth::user()->id;
+        $topic->reply_count++;
+        $topic->updated_at = Carbon::now()->toDateTimeString();
+        $topic->save();
+
+        Auth::user()->increment('reply_count', 1);
 	}
 
 	/**
