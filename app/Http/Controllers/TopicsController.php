573bcf99d2925a7f130a7457fce434c3054dc31c
yiyangyi <yiyangyigg@gmail.com>
2015-03-20 15:08:43 +0800

    Add the method to pin, sink and destroy a topic.

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -4,23 +4,23 @@ use App\Http\Requests;
 use App\Http\Requests\CreateTopicRequest;
 use App\Http\Controllers\Controller;
 
+use App\Services\Markdown;
 use App\Topic;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TopicsController extends Controller {
-
-	protected $topic;
 
 	/**
 	 * Instantiate a new TopicController instance.
 	 * @param Topic $topic 
 	 */
-	public function __construct(Topic $topic)
+	public function __construct()
 	{
 		parent::__construct();
 
 		$this->middleware('auth', ['except' => 'index', 'show']);
-		$this->topic = $topic;
 	}
 
 	/**
@@ -48,8 +48,22 @@ class TopicsController extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function store(CreateTopicRequest $request)
+	public function store(CreateTopicRequest $request, Topic $topic, Markdown $markdown)
 	{
+        $user_id = Auth::user()->id;
+        $body_original = $request->input('body_original');
+        $body = $markdown->convertMarkdownToHtml($body_original);
+        $created_at = Carbon::now()->toDateTimeString();
+        $updated_at = Carbon::now()->toDateTimeString();
+        $topic = Topic::create(['user_id'       => $user_id
+                                'body'          => $body,
+                                'body_original' => $body_original,
+                                'created_at'    => $created_at,
+                                'updated_at'    => $updated_at]);
+        if (!$topic) {
+            redirect('topic.create');
+        }
+        Auth::user()->increment('topic_count', 1);
 		return redirect('topics.index');
 	}
 
@@ -82,6 +96,21 @@ class TopicsController extends Controller {
 		return view('topics.edit', compact('topic', 'nodes', 'node'));
 	}
 
+
+    /**
+     * Add an append to the original topic.
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function append($id)
+    {
+        $topic = Topic::findOrFail($id);
+        $this->adminOrAuthorPermissionRequired($topic->user_id);
+
+
+    }
+
 	/**
 	 * Update the specified resource in storage.
 	 *
@@ -101,7 +130,23 @@ class TopicsController extends Controller {
 	 */
 	public function destroy($id)
 	{
-		//
+		$topic = Topic::findOrFail($id);
+        $topic->destroy();
+        return redirect('topics/index')->with('Deleted successfully!');
 	}
+
+    public function pin($id)
+    {
+        $topic = Topic::findOrFail($id);
+        $topic->order > 0 ? $topic->decrement('order', 1) : $topic->increment('order', 1);
+        return redirect('topics/index');
+    }
+
+    public function sink($id)
+    {
+        $topic = Topic::findOrFail($id);
+        $topic->order >= 0 ? $topic->decrement('order', 1) : $topic->increment('order', 1);
+        return redirect('topic/index');
+    }
 
 }
