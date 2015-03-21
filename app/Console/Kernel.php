bc68e14c31cf8e77aa02c6f004a1724f3c61c8fb
yiyangyi <yiyangyigg@gmail.com>
2015-03-21 23:11:22 +0800

    Add some boilerplate scheduling artisan commands to run some scheduled jobs.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,38 @@ class Kernel extends ConsoleKernel {
 	{
 		$schedule->command('inspire')
 				 ->hourly();
+	    $schedule->call(function()
+	    {
+	    	// Do some task...
+
+	    })->hourly();
+	    $schedule->exec('composer self-update')->daily();
+	    $schedule->command('foo')->cron('* * * * *');
+	    $schedule->command('foo')->everyFiveMinutes();
+	    $schedule->command('foo')->everyTenMinutes();
+	    $schedule->command('foo')->everyThirtyMinutes();
+	    $schedule->command('foo')->daily();
+	    $schedule->command('foo')->dailyAt('15:00');
+	    $schedule->command('foo')->twiceDaily();
+	    $schedule->command('foo')->weekdays();
+	    $schedule->command('foo')->weekly();
+	    $schedule->command('foo')->weeklyOn(1, '8:00');
+	    $schedule->command('foo')->monthly();
+	    $schedule->command('foo')->mondays();
+        $schedule->command('foo')->tuesdays();
+        $schedule->command('foo')->thursdays();
+        $schedule->command('foo')->fridays();
+        $schedule->command('foo')->saturdays();
+        $schedule->command('foo')->sundays();
+        $schedule->command('foo')->monthly()->environments('production');
+        $schedule->command('foo')->monthly()->evenInMaintenanceMode();
+        $schedule->command('foo')->monthly()->when(function()
+		{
+		    return true;
+		});
+		$schedule->command('foo')->sendOutputTo($filePath)->emailOutputTo('foo@example.com');
+		$schedule->command('foo')->sendOutputTo($filePath);
+		$schedule->command('foo')->thenPing($url);
 	}
 
 }
