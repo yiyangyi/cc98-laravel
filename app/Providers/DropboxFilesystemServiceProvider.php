db93a5a8d3d877907538cf3c3acebb7fd7f88f30
yiyangyi <yiyangyigg@gmail.com>
2015-03-23 11:35:24 +0800

    Add the dropbox adapter to extend the choice to use dropbox for backing up.

---
+++ app/Providers/DropboxFilesystemServiceProvider.php
@@ -0,0 +1,27 @@
+<?php namespace App\Providers;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Dropbox\DropboxAdapter;
+use League\Flysystem\Filesystem;
+use Dropbox\Client;
+
+class DropboxFilesystemServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+        Storage::extend('dropbox', function()
+        {
+            $client = new Client($config["accessToken"], config["ClientIdentifier"]);
+            $adapter = new DropboxAdapter($client);
+            $filesystem = new Filesystem($adapter);
+            return $filesystem;
+        });
+	}
+
+}
