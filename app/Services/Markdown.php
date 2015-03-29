11444d33e9c352470cd7076cf6d4a6ade4564534
yiyangyi <yiyangyigg@gmail.com>
2015-03-29 21:40:16 +0800

    Add the method to enable converting user input markdown into html.

--- app/Services/Markdown.php
+++ app/Services/Markdown.php
@@ -1,10 +1,14 @@
 <?php namespace App\Services;
 
+use Parsedown;
+
 class Markdown {
+
+    protected $parsedown;
 
     public function __construct()
     {
-
+        $this->parsedown = new Parsedown();
     }
 
     public function convertHtmlToMarkdown($html)
@@ -14,7 +18,8 @@ class Markdown {
 
     public function convertMarkdownToHtml($markdown)
     {
-
+        $convertedHtml = $this->parsedown->text($markdown);
+        return $convertedHtml;
     }
 
 }
